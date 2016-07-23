998393191065eb9afc15f76f376ee66862353684
lsx <lsx@163.com>
2016-07-23 14:50:43 +0800

    MainActivity的Button重构到成员变量

--- app/src/main/java/com/li/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/li/activitylifecycle/MainActivity.java
@@ -12,16 +12,18 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     public static final String BOOK_NAME_OF_CATEGORY_PHYSICAL = "bookNameOfCategoryPhysical";
+    private Button mButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
-        Button button = (Button) findViewById(R.id.activity_main_button);
+        mButton = (Button) findViewById(R.id.activity_main_button);
         final EditText editText = (EditText) findViewById(R.id.activity_main_edit_text);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
